104755455075e8098b54cc13e1168e61c40dc67d
Brandon Ahrens <b-ahrens@wiu.edu>
2021-02-26 01:10:17 -0600

    added debug value as environment variable

--- Workout Buddy/workout_api/workout_api/settings.py
+++ Workout Buddy/workout_api/workout_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv("DEBUG", 0))) #defaults to false.
 
 #added phone host
 ALLOWED_HOSTS = [
